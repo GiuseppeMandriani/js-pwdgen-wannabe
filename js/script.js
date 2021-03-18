094e50a7440b6362066ec5db80722ea11d42e125
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-18 16:01:34 +0100

    start js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// PASSWORD GENERATOR
+
+// 1 PROMPT CHIEDERE NOME
+
+var firstName = prompt('Nome');
+console.log(firstName);
+
+// 2 PROMPT CHIEDERE COGNOME
+
+var lastName = prompt('Cognome');
+console.log(lastName);
+
+// 3 PROMPT CHIEDERE ANNI
+
+var age = parseInt(prompt('Quanti anni hai?')); 
+console.log(age);
+console.log(typeof(age));
+
+
+
+/* PROMPT Metodo per ottenere informazioni dall'utente
+
+    - Può essere assegnato direttamente alla variabile */
+
+
+
+
+
+/* Regole Variabili
+
+    - Keyword
+    - nomeVariabile
+    - assegnazione =
+    - valore
+
+    Esempio con variabile numerica*/
+
+/*var age = 31;
+console.log(age);*/ //Per visualizzare in devTools la console
+
+
+    //Esempio con concatenazione di string
+
+/*var nome = "Giuseppe";
+var cognome = "Mandriani";
+var all = nome + " " + cognome;
+console.log(typeof(all));
+console.log(all);*/
+
+
+
+
+
+// 1 Ottenere l'elemento html by ID 
+// Settarne il testo interno
+
+//document.getElementById('title').innerHTML = 'Create a Password';
